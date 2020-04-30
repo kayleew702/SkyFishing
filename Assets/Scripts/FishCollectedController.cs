0e9874aab588dea43ba5abf7a1faaf0c8900e905
ellenmowat <ellenmowat@users.noreply.github.com>
2020-04-30 16:54:18 -0700

    Diving Mechanics and fish score controller

---
+++ Assets/Scripts/FishCollectedController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FishCollectedController : MonoBehaviour
+{
+    public int fishCollected = 0;
+
+    public void UpdateFishCollected()
+    {
+        GetComponent<Text>().text = "Fish Collected:  " + fishCollected;
+    }
+}
