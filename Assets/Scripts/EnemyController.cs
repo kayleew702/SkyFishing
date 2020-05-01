23262e280c8d5f0ef2dfd9ab6929190bb4082626
bkioko <59579466+bkioko@users.noreply.github.com>
2020-04-30 17:02:06 -0700

    fish spawner

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,26 +6,18 @@ public class EnemyController : MonoBehaviour
 {
     public float speed;
     private Rigidbody2D rb;
-    //private Vector2 screenBounds;
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = new Vector2(0, speed);
-        //screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        //if (Camera.main.WorldToViewportPoint(transform.position).y > 0)
-        //    Debug.Log("Enemy Destroyed");
-        //    Destroy(this.gameObject);
-        //if (transform.position.y < screenBounds.y * 2)
-        //    Debug.Log("Enemy Destroyed");
-        //    Destroy(this.gameObject);
         if (transform.position.y > Camera.main.WorldToViewportPoint(transform.position).y)
         {
             Debug.Log("Enemy destroyed");
